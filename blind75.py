ac0b27821c20b6e6beb832d05f9b7b207949056a
Suprim1043 <Suprim1043@gmail.com>
2024-10-26 11:00:48 -0500

    2 pointers threesum. 10/26/2024.

--- blind75.py
+++ blind75.py
@@ -41,5 +41,25 @@ def anagrams(strs):
         newDict[sorting].append(string)
     return (newDict.values())
 
-print(anagrams(strs))
 
+
+###################
+#3 sum 
+
+
+
+nums = [-1,0,1,2,-1,-4]
+def threeSum(nums):
+    result = set()
+    nums.sort()
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            for k in range(j+1, len(nums)):
+                if nums[i] + nums[j] + nums[k] == 0 :
+                    temp = [nums[i],nums[j],nums[k]]
+                    result.add(tuple(temp))
+            
+                        
+    return [list(i) for i in result]
+
+print(threeSum(nums))
