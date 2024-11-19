54a0d70d47da286d67936c2f5cbc3ae6a5e953e2
Suprim1043 <Suprim1043@gmail.com>
2024-11-18 21:24:05 -0600

    max area problem.

--- blind75.py
+++ blind75.py
@@ -277,6 +277,22 @@ def threesome(nums):
     return result
 
 
+height = [1,7,2,5,4,7,3,6]
 
-print(threesome(nums))
-    
+def maxArea(height):
+
+    result = 0
+    l,r = 0, len(height) - 1
+
+    while l < r:
+        area = (r-l) * min(height[l], height[r])
+        result = max(result, area)
+
+        if height[l] < height[r]:
+            l += 1
+        else:
+            r -= 1
+    return result
+
+
+print(maxArea(height))
