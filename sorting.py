06b7b93a37e57061f93f45599a88f51682f0bd54
Suprim1043 <Suprim1043@gmail.com>
2025-01-08 20:38:33 -0600

    insertion sort
    insertion sort learning.

--- sorting.py
+++ sorting.py
@@ -7,7 +7,19 @@ def bubbleSort(array):
 
 array = [8,7,3,4,56,2]
 
-print(bubbleSort(array))
+
+def insertionSort(array):
+
+    n = len(array)
+    for i in range(n):
+        j = i - 1
+        while j >= 0 and array[j] > array[j+1]:
+            array[j], array[j+1] = array[j+1], array[j]
+            j -= 1
+    return array
+
+print(insertionSort(array))
+
 
 
 
